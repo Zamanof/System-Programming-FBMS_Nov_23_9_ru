3dbccd375562c9ff48b3490a2d1b83714344b76f
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-09-27 12:10:17 +0400

    TPL

---
+++ SP 06. TPL/Program.cs
@@ -0,0 +1,50 @@
+﻿// TPL - Task Parallel Library
+
+// Thread -> ThreadPool -> TPL
+
+Console.WriteLine("Start of code...");
+
+//Task task1 = new Task(() =>
+//{
+//    TaskMethod("Task 1");
+//});
+
+//Task task2 = new Task(() =>
+//{
+//    TaskMethod("Task 2");
+//});
+
+//Task task3 = Task.Run(() =>
+//{
+//    TaskMethod("Task 3");
+//});
+
+//Task task4 = Task.Factory.StartNew(() =>
+//{
+//    TaskMethod("Task 4");
+//});
+
+var task5 = new Task(() => {
+    TaskMethod("Task 5");
+}, TaskCreationOptions.LongRunning);
+
+//task1.Start();
+//task2.Start();
+task5.Start();
+
+
+//task1.Wait();
+//task2.Wait();
+Console.ReadKey();
+Console.WriteLine("End of code...");
+
+
+void TaskMethod(string message)
+{
+    Console.WriteLine("Task Method start...");
+    Console.WriteLine(@$"Task - {message} is running.
+Id - {Thread.CurrentThread.ManagedThreadId}
+IsThreadPool - {Thread.CurrentThread.IsThreadPoolThread}
+IsBackgroundThread - {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine("Task Method end...\n");
+}
---
+++ SP 06. Task result/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Start of code.");
+var task = Task.Run(() => TaskReturnMethod("Task1", 5));
+
+Console.WriteLine(task.Result);
+Console.WriteLine("End of code.");
+
+int TaskReturnMethod(string message, int second)
+{
+    Console.WriteLine("TaskReturnMethod start...");
+    Console.WriteLine(@$"Task - {message} is running.
+Id - {Thread.CurrentThread.ManagedThreadId}
+IsThreadPool - {Thread.CurrentThread.IsThreadPoolThread}
+IsBackgroundThread - {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine("TaskReturnMethod end...\n");
+    Thread.Sleep(second * 1000);
+    return second * 10;
+}
+
